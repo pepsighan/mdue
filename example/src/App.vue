30c52a8de86a701308ba11767ffeb7387a2e1503
Sharad Chand <pepsighan@sharadchand.com>
2020-08-08 17:22:51 +0545

    Add example

---
+++ example/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <ab-testing></ab-testing>
+  </div>
+</template>
+
+<script>
+import { AbTesting } from 'mdue';
+
+export default {
+  name: 'App',
+  components: {
+    AbTesting,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: blue;
+  margin-top: 60px;
+  font-size: 1.5rem;
+}
+</style>
