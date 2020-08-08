8560352b728aa42415c8f69d5950ec457220f336
Sharad Chand <pepsighan@sharadchand.com>
2020-08-08 19:24:58 +0545

    All types are same. Saves on compile time.

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const components = meta.map((icon) => {
   const name = pascalCase(icon.name);
 
   return `const path${name} = /*#__PURE__*/createVNode("path", { d: "${icon.path}" }, null, -1 /* HOISTED */);
-export const ${name} = defineComponent({
+export const ${name}: IconComponent = defineComponent({
   name: '${name}',
   render: () => {
     return (openBlock(), createBlock('svg', svgCommonProps, [path${name}]));
@@ -24,7 +24,63 @@ export const ${name} = defineComponent({
 });
 
 // Export the vue components from the index.ts.
-const indexFileContent = `import { defineComponent, openBlock, createBlock, createVNode } from 'vue';
+const indexFileContent = `import { defineComponent, openBlock, createBlock, createVNode, ComponentPublicInstance, VNodeProps, AllowedComponentProps, ComponentCustomProps, ComponentOptionsBase, ComponentOptionsMixin } from 'vue';
+
+// This is here to optimize the type generation performance. All the components have the same type so.
+type IconComponent = (new () => ComponentPublicInstance<
+  {},
+  {},
+  {},
+  {},
+  {},
+  Record<string, any>,
+  VNodeProps & AllowedComponentProps & ComponentCustomProps,
+  ComponentOptionsBase<
+    {},
+    {},
+    {},
+    {},
+    {},
+    ComponentOptionsMixin,
+    ComponentOptionsMixin,
+    Record<string, any>,
+    string
+  >
+>) &
+  ComponentOptionsBase<
+    {},
+    {},
+    {},
+    {},
+    {},
+    ComponentOptionsMixin,
+    ComponentOptionsMixin,
+    Record<string, any>,
+    string
+  > & {
+    props?: undefined;
+  } & ThisType<
+    ComponentPublicInstance<
+      {},
+      {},
+      {},
+      {},
+      {},
+      Record<string, any>,
+      Readonly<{}>,
+      ComponentOptionsBase<
+        {},
+        {},
+        {},
+        {},
+        {},
+        ComponentOptionsMixin,
+        ComponentOptionsMixin,
+        Record<string, any>,
+        string
+      >
+    >
+  >;
 
 const svgCommonProps = {
   xmlns: 'http://www.w3.org/2000/svg',
